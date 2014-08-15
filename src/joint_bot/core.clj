749e9bf8ad285d667a9d6eddbd7b572ca6db110c
Lester <john.lester.gerardo@.joint.no>
2014-08-15 09:45:14 +0200

    Who the man

--- src/joint_bot/core.clj
+++ src/joint_bot/core.clj
@@ -116,6 +116,7 @@
   (let [command (clojure.string/replace body #"@jointbot " "")]
     (cond (starts-with command "latest artifact") (latest-artifact (last (clojure.string/split command #" ")))
           (starts-with command "spank") (str "Come on over here " (clojure.string/split command #" ") " and I'll give you a real spanking!")
+	  (starts-with command "who the man") (str "Lester tha man")	
           :else (str "I don't know how to " command))))
 
 
