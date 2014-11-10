60577cfc02ee48c5d60d3675cf890331712cb82e
Erik Assum <erik@iterate.no>
2014-11-10 15:49:27 +0100

    Removing code

--- src/mybot/core.clj
+++ src/mybot/core.clj
@@ -1,41 +1,53 @@
 (ns mybot.core
     (:require [mybot.xmpp :as xmpp]
               [mybot.util :as util]
-              [mybot.jira :as jira]              
-              [mybot.config :as config]))
+              [clj-http.client :as client]))
 
-(def msgs (atom []))
+(def xmpp {:host "localhost"
+          :port 5222
+          :username "sausagebot"
+          :password "sausage"
+          :resource "Mr. Sausage"
+          :room "clojure@conference.clojutre" })
 
-(defn from-me [{:keys [from]}]
-  (util/contains from (str (config/getenv [:xmpp :room]) "/" (config/getenv [:xmpp :username]))))
-
-(defn to-me [{:keys [body]}]
-  (util/contains body (config/getenv [:xmpp :name])))
-
-(defn tell-where []
-  (let [{:keys [host port]} (util/runtime-info)]
-    (str "Oh, I'm running on " host ":" port)))
-
-(defn remove-handle [body]
-  (util/remove-str body (str (config/getenv [:xmpp :name]) " ")))
+(defn issue [issue]
+  (let [url (str "http://localhost:8080/rest/api/latest/issue/" issue)]
+    (:body (client/get url
+                       {:as :json
+                        :basic-auth ["sausagebot" "sausage"]
+                        :content-type :json
+                        }))))
 
 (defn format-issue [issue]
-  (str (jira/get-link issue) " "
+  (str "http://localhost:8080/browse/" (:key issue) " "
        (get-in issue [:fields :summary]) ", status: "
        (get-in issue [:fields :status :name]) ", assignee: "
        (get-in issue [:fields :assignee :name])))
 
 (defn show-issue [issue-id]
   (-> issue-id
-      (jira/issue)
+      (issue)
       (format-issue)))
+
+(defn from-me [{:keys [from]}]
+  (util/contains from (str (:room xmpp) "/" (:username xmpp))))
+
+(defn to-me [{:keys [body]}]
+  (util/contains body (:username xmpp)))
+
+(defn tell-where []
+  (let [{:keys [host port]} (util/runtime-info)]
+    (str "Oh, I'm running on " host ":" port)))
+
+(defn remove-handle [body]
+  (util/remove-str body (str "@" (:username xmpp) " ")))
 
 (defn handle-command [{:keys [body from] :as message}]
   (let [command (remove-handle body)]
-    (cond (util/starts-with body "where are you running") (tell-where))))
+    (cond (util/starts-with command "where are you running") (tell-where))))
 
 (defn handle-noise [{:keys [body]}]
-  (let [issue (util/matches body (config/getenv [:jira :issue-matcher]))]
+  (let [issue (util/matches body #".*(CLOJ-\d+).*")]
     (cond (util/contains body "clojure") "clojure rocks!"
           issue (show-issue issue))))
   
@@ -43,6 +55,8 @@
   (if (to-me message)
     (handle-command message) 
     (handle-noise message)))
+
+(def msgs (atom []))
 
 (defn store-message [handler]
   (fn [message]
@@ -53,15 +67,13 @@
   (fn [message]
     (when-not (from-me message)
       (handler message))))
-    
+
 (comment 
 (reset! msgs [])
-(def chat (xmpp/connect (config/getenv [:xmpp])))
-(def clojure-room (xmpp/join chat
-                             (config/getenv [:xmpp :room])
-                             (config/getenv [:xmpp :username])))
+(def chat (xmpp/connect xmpp))
+(def clojure-room (xmpp/join chat (:room xmpp) (:username xmpp)))
 
-(.sendMessage clojure-room "hello!")
+(.sendMessage clojure-room "Hello! Clojutre!!")
 
 (xmpp/add-message-listener (store-message (dont-reply-to-self #'handle-chatter)) clojure-room)
 )
