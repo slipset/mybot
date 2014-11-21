1dd59c79eeb95ecf8e1395d780f9749949e7e747
Erik Assum <erik@iterate.no>
2014-11-21 22:09:59 +0100

    Upgraded to xmpp-clj-0.0.2

--- project.clj
+++ project.clj
@@ -7,5 +7,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-http "0.7.8"]
-                 [net.assum/xmpp-clj "0.0.1"]]
+                 [net.assum/xmpp-clj "0.0.2"]]
    :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
